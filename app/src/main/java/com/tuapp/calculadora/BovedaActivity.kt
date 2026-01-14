41f90c91aada9b9d630f36568f4e5f8defa3f755
TRISKANV <borrarcuenta0800@gmail.com>
2026-01-14 19:12:21 -0300

    Create BovedaActivity.kt

---
+++ app/src/main/java/com/tuapp/calculadora/BovedaActivity.kt
@@ -0,0 +1,14 @@
+package com.tuapp.calculadora
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class BovedaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boveda)
+
+        // Aquí podrías agregar un botón para "Importar Fotos" más adelante
+    }
+}
