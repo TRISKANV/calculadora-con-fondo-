f9fb034f64b9b2a7481a3b3898c64ddb4d6370af
TRISKANV <borrarcuenta0800@gmail.com>
2026-01-14 21:17:05 -0300

    Create FotoAdapter.kt

---
+++ app/src/main/java/com/tuapp/calculadora/FotoAdapter.kt
@@ -0,0 +1,27 @@
+package com.tuapp.calculadora
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import java.io.File
+
+class FotoAdapter(private val context: Context, private val listaFotos: List<File>) : BaseAdapter() {
+
+    override fun getCount(): Int = listaFotos.size
+    override fun getItem(position: Int): Any = listaFotos[position]
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_foto, parent, false)
+        val imageView = view.findViewById<ImageView>(R.id.ivItemFoto)
+        
+        // Cargamos la imagen desde el archivo privado
+        imageView.setImageURI(Uri.fromFile(listaFotos[position]))
+        
+        return view
+    }
+}
