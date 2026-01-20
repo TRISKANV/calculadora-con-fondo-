f50f2c83832e6573f8a91bb496a27e4dc1fe17f8
TRISKANV <borrarcuenta0800@gmail.com>
2026-01-20 17:27:02 -0300

    Update Nota.kt

--- app/src/main/java/com/tuapp/calculadora/Nota.kt
+++ app/src/main/java/com/tuapp/calculadora/Nota.kt
@@ -1,10 +1,15 @@
 package com.tuapp.calculadora
 
-import java.util.UUID
+import java.text.SimpleDateFormat
+import java.util.*
 
 data class Nota(
     val id: String = UUID.randomUUID().toString(),
-    var titulo: String,
     var contenido: String,
     val fecha: Long = System.currentTimeMillis()
-)
+) {
+    fun getFechaFormateada(): String {
+        val sdf = SimpleDateFormat("dd MMM, hh:mm a", Locale.getDefault())
+        return sdf.format(Date(fecha))
+    }
+}
