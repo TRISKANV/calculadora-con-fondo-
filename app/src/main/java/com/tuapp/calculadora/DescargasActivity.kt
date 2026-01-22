79f9ef2bef2050cd435b762fe8c1f64f5c72258e
TRISKANV <borrarcuenta0800@gmail.com>
2026-01-22 19:25:51 -0300

    Create DescargasActivity.kt

---
+++ app/src/main/java/com/tuapp/calculadora/DescargasActivity.kt
@@ -0,0 +1,11 @@
+package com.tuapp.calculadora
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DescargasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
+}
