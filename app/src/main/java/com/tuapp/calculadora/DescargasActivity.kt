7e275400b9050c3006c3dc06e477f4a360a584a6
TRISKANV <borrarcuenta0800@gmail.com>
2026-01-22 22:21:38 -0300

    Update DescargasActivity.kt

--- app/src/main/java/com/tuapp/calculadora/DescargasActivity.kt
+++ app/src/main/java/com/tuapp/calculadora/DescargasActivity.kt
@@ -7,5 +7,10 @@ class DescargasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
+        
+        supportActionBar?.hide()
+
+        
+        setContentView(R.layout.activity_descargas)
     }
 }
