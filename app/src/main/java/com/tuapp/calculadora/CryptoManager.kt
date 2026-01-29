bb4e8a43f646d9a113f91da5377c65ef9d7fff07
TRISKANV <borrarcuenta0800@gmail.com>
2026-01-29 20:21:31 -0300

    Create CryptoManager.kt

---
+++ app/src/main/java/com/tuapp/calculadora/CryptoManager.kt
@@ -0,0 +1,58 @@
+package com.tuapp.calculadora
+
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import java.io.*
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.GCMParameterSpec
+
+class CryptoManager {
+
+    private val keyStore = KeyStore.getInstance("AndroidKeyStore").apply { load(null) }
+    
+    private fun getKey(): SecretKey {
+        val existingKey = keyStore.getEntry("secret_key", null) as? KeyStore.SecretKeyEntry
+        return existingKey?.secretKey ?: createKey()
+    }
+
+    private fun createKey(): SecretKey {
+        return KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore").apply {
+            init(KeyGenParameterSpec.Builder("secret_key", 
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .build())
+        }.generateKey()
+    }
+
+    fun encrypt(inputStream: InputStream, outputStream: OutputStream) {
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        cipher.init(Cipher.ENCRYPT_MODE, getKey())
+        
+        //
+        outputStream.write(cipher.iv.size)
+        outputStream.write(cipher.iv)
+
+        val buffer = ByteArray(8192)
+        var bytesRead: Int
+        val cipherOutputStream = javax.crypto.CipherOutputStream(outputStream, cipher)
+        while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+            cipherOutputStream.write(buffer, 0, bytesRead)
+        }
+        cipherOutputStream.close()
+    }
+
+    fun decrypt(inputStream: InputStream): ByteArray {
+        val ivSize = inputStream.read()
+        val iv = ByteArray(ivSize)
+        inputStream.read(iv)
+
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        cipher.init(Cipher.DECRYPT_MODE, getKey(), GCMParameterSpec(128, iv))
+        
+        return cipher.doFinal(inputStream.readBytes())
+    }
+}
